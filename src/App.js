3255899b4e49625af5443959a942f48202313c44
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-05 01:02:29 +0200

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import "./weatherapp.css";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="App">
+    <div className="App">
+      <div className="container">
         <Search />
+
+        <footer className="link">
+          <a
+            href="https://github.com/CarlaAll/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-Source Code
+          </a>{" "}
+          by Carla Allenbach
+        </footer>
       </div>
-      <span className="link">
-        <a
-          href="https://github.com/CarlaAll/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-Source Code
-        </a>{" "}
-        by Carla Allenbach
-      </span>
     </div>
   );
 }
