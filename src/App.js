483641bcd0f9f3fad1287ff0316840209f0b1f96
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-05 00:42:42 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ export default function App() {
       <div className="App">
         <Search />
       </div>
+      <span className="link">
+        <a href="https://github.com/CarlaAll/react-weather-app" target="_blank">
+          Open-Source Code
+        </a>{" "}
+        by Carla Allenbach
+      </span>
     </div>
   );
 }
