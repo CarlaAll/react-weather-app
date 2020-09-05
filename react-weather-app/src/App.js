2485185aa08803d281776bafd3da44508d69ec8d
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-05 10:10:10 +0200

    update

---
+++ react-weather-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <Search />
+    </div>
+  );
+}
