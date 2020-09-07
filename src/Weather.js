cd090e03c6e4ea04cd3a87c0c98a6ab78e5dde03
CarlaAll <69358793+CarlaAll@users.noreply.github.com>
2020-09-07 20:16:22 +0200

    update SearchEngine function

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
   function handleResponse(response) {
     console.log(response.data);
@@ -14,16 +15,31 @@ export default function Weather(props) {
       description: response.data.weather[0].description,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      city: response.data.name,
-      date: new date(response.data.dt * 1000),
+      city: `${response.data.name}, ${response.data.sys.country}`,
+      date: new Date(response.data.dt * 1000),
       icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
     });
+  }
+
+  function search(event) {
+    const apiKey = "0b04f7882b2a886e7942aa9e854e4071";
+    const units = "metric";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+    search();
   }
 
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -31,6 +47,7 @@ export default function Weather(props) {
                 placeholder="Enter a city.."
                 className="form-control"
                 autoFocus="on"
+                onChange={handleCityChange}
               />
             </div>
             <div className="col-3">
@@ -38,50 +55,11 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-
-        <div className="row mt-5">
-          <div className="col-6">
-            <div className="clearfix">
-              <img
-                src={weatherData.icon}
-                alt={weatherData.description}
-                className="float-left"
-              />
-              <div className="float-left">
-                <span className="temperature">
-                  {Math.round(weatherData.temperature)}
-                </span>
-                <span className="units">°C | F </span>
-              </div>
-            </div>
-            <ul>
-              <li>
-                <h3>{weatherData.city}</h3>
-              </li>
-              <li className="time">
-                <formattedDate date={weatherData.date} />
-              </li>
-            </ul>
-          </div>
-          <div className="col-6">
-            <div className="advice">
-              <strong>Today's Advice:</strong> <br /> take an umbrella with you.
-            </div>
-            <ul className="additional">
-              <li className="text-capitalize">{weatherData.description}</li>
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo info={weatherData} />
       </div>
     );
   } else {
-    const apiKey = "0b04f7882b2a886e7942aa9e854e4071";
-    const units = "metric";
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
+    return "Loading...";
   }
-
-  return "Loading...";
 }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,42 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo row mt-5">
+      <div className="col-6">
+        <div className="clearfix">
+          <img
+            src={props.info.icon}
+            alt={props.info.description}
+            className="float-left"
+          />
+          <div className="float-left">
+            <span className="temperature">
+              {Math.round(props.info.temperature)}
+            </span>
+            <span className="units">°C | F </span>
+          </div>
+        </div>
+        <ul>
+          <li>
+            <h3>{props.info.city}</h3>
+          </li>
+          <li className="time">
+            <FormattedDate date={props.info.date} />
+          </li>
+        </ul>
+      </div>
+      <div className="col-6">
+        <div className="advice">
+          <strong>Today's Advice:</strong> <br /> take an umbrella with you.
+        </div>
+        <ul className="additional">
+          <li className="text-capitalize">{props.info.description}</li>
+          <li>Humidity: {props.info.humidity}%</li>
+          <li>Wind: {props.info.wind} km/h</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
